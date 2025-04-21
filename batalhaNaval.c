8a5796f2a1dec0e87805f41c4a3c5dcf24561ffe
Joel Almeida <joel.almeida.sa@gmail.com>
2025-04-20 21:35:18 -0300

    Implement basic board rendering and ship placement logic

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,11 +4,54 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
+void renderBoard(int board[10][10]) {
+    for (int horizontalIndex = 0; horizontalIndex < 10; horizontalIndex++) {
+        for (int verticalIndex = 0; verticalIndex < 10; verticalIndex++) {
+            printf("%d  ", board[horizontalIndex][verticalIndex]);
+        }
+        printf("\n");
+    }
+    printf("\n\n");
+}
+
+void fillBoard(int board[10][10]) {
+    for (int horizontalIndex = 0; horizontalIndex < 10; horizontalIndex++) {
+        for (int verticalIndex = 0; verticalIndex < 10; verticalIndex++) {
+            board[horizontalIndex][verticalIndex] = 0;
+        }
+    }
+}
+const int createVertically = 1;
+
+void createShip(int board[10][10], const int shipStartX, const int shipStartY, const int createVertically) {
+    const int shipSize = 3;
+
+    if (createVertically) {
+        for (int verticalIndex = shipStartY; verticalIndex < shipStartY + shipSize; verticalIndex++) {
+            board[verticalIndex][shipStartX] = 3;
+        }
+    } else {
+        for (int horizontalIndex = shipStartX; horizontalIndex < shipStartX + shipSize; horizontalIndex++) {
+            board[shipStartY][horizontalIndex] = 3;
+        }
+    }
+}
+
 int main() {
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    int board[10][10];
+
+    fillBoard(board);
+    renderBoard(board);
+
+    createShip(board, 0, 0, 0);
+    renderBoard(board);
+
+    createShip(board, 9, 7, 1);
+    renderBoard(board);
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
